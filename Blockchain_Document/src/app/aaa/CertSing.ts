f3269cb6ee858d426ae8b3abbb6792d703783445
Ahmed-Achtatar <ahmed.achtatar@usmba.ac.ma>
2022-02-23 14:20:13 +0100

    more bugs

--- Blockchain_Document/src/app/aaa/CertSing.ts
+++ Blockchain_Document/src/app/aaa/CertSing.ts
@@ -4,43 +4,43 @@ import * as fs from "fs";
 var qr = require('qr-image');
 PDFNet.initialize('demo:omaralami230@gmail.com:7b01f4ab020000000092768e068e8737e8b8c939452e7892e0470df170');
 class DocumentSign {
-	public doc: any;
+	public docpath: any;
 	public certificate: any;
 	public file: any;
 	public timestamp: any;
     // ---------------- Preparation --------------------
     // Récuperer le document dont on veut signer
-    constructor(doc : any, certificate : any) {
-        PDFNet.initialize('demo:omaralami230@gmail.com:7b01f4ab020000000092768e068e8737e8b8c939452e7892e0470df170');
-        this.doc = PDFNet.PDFDoc.createFromFilePath(doc);
+    constructor(docpath : string, certificate : string) {
+        this.docpath = docpath;
         this.certificate = certificate;
         this.timestamp = Date.now();
 
     }
     async Sign() {
-        await PDFNet.initialize('demo:omaralami230@gmail.com:7b01f4ab020000000092768e068e8737e8b8c939452e7892e0470df170');
-            let page1 = this.doc.getPage(1);
+      await PDFNet.initialize('demo:omaralami230@gmail.com:7b01f4ab020000000092768e068e8737e8b8c939452e7892e0470df170');
+        let doc = await PDFNet.PDFDoc.createFromFilePath(this.docpath);
+            let page1 = doc.getPage(1);
             let builder = PDFNet.ElementBuilder.create();
             // Writer est le responsable d'ajouter du texte,d'image,... à un block de builder
             let writer = PDFNet.ElementWriter.create();
 
             /*---------------- Ajout d'une E-Signature --------------------*/
 
-            let certification_sig_field = this.doc.createDigitalSignatureField('Certificate');
+            let certification_sig_field =  await doc.createDigitalSignatureField('Certificate');
             certification_sig_field.setDocumentPermissions(PDFNet.DigitalSignatureField.DocumentPermissions.e_annotating_formfilling_signing_allowed);
-            let widgetAnnot = PDFNet.SignatureWidget.createWithDigitalSignatureField(this.doc, new PDFNet.Rect(parseFloat((page1.getPageWidth()).toString()) - 200, parseFloat((page1.getPageHeight()).toString()) - 750, parseFloat((page1.getPageWidth()).toString()) - 30, parseFloat((page1.getPageHeight()).toString()) - 800), certification_sig_field);
-            page1.annotPushBack(widgetAnnot);
+            let widgetAnnot = await PDFNet.SignatureWidget.createWithDigitalSignatureField(doc, new PDFNet.Rect(parseFloat(((await page1).getPageWidth()).toString()) - 200, parseFloat(((await page1).getPageHeight()).toString()) - 750, parseFloat(((await page1).getPageWidth()).toString()) - 30, parseFloat(((await page1).getPageHeight()).toString()) - 800), certification_sig_field);
+            (await page1).annotPushBack(widgetAnnot);
             var fields_to_lock = ['asdf_test_field'];
             certification_sig_field.setFieldPermissions(PDFNet.DigitalSignatureField.FieldPermissions.e_include, fields_to_lock);
             certification_sig_field.certifyOnNextSave(this.certificate, 'ahmedahmed');
             // Ajouter les Permissions à la signature
             (await
         // Ajouter les Permissions à la signature
-        writer).beginOnPage(page1);
-            let element = (await builder).createTextBeginWithFont(await PDFNet.Font.create(this.doc, PDFNet.Font.StandardType1Font.e_times_roman), 20);
+        writer).beginOnPage(await page1);
+            let element = (await builder).createTextBeginWithFont(await PDFNet.Font.create(doc, PDFNet.Font.StandardType1Font.e_times_roman), 20);
             (await writer).writeElement(await element);
             element = (await builder).createNewTextRun('');
-            (await element).setTextMatrixEntries(0.5, 0, 0, 0.5, parseFloat((page1.getPageWidth()).toString()) - 190, parseFloat((page1.getPageHeight()).toString()) - 760);
+            (await element).setTextMatrixEntries(0.5, 0, 0, 0.5, parseFloat(((await page1).getPageWidth()).toString()) - 190, parseFloat(((await page1).getPageHeight()).toString()) - 760);
             (await writer).writeElement(await element);
 
             var options = {
@@ -56,29 +56,30 @@ class DocumentSign {
             let qr_svg = qr.imageSync("omar", options);
 
             let signatureDate = certification_sig_field.getSigningTime();
-            element = (await builder).createNewTextRun('Date: ' + signatureDate.year + '/' + signatureDate.month + '/' + signatureDate.day + ' at ' + signatureDate.hour + ':' + signatureDate.minute + ':' + signatureDate.second);
+            element = (await builder).createNewTextRun(`Date: ${(await signatureDate).year}/${(await signatureDate).month}/${(await signatureDate).day} at ${(await signatureDate).hour}:${(await signatureDate).minute}:${(await signatureDate).second}`);
+
             // element.setPosAdjustment(15);
             (await
         // element.setPosAdjustment(15);
-        element).setTextMatrixEntries(0.5, 0, 0, 0.5, parseFloat((page1.getPageWidth()).toString()) - 190, parseFloat((page1.getPageHeight()).toString()) - 775);
+        element).setTextMatrixEntries(0.5, 0, 0, 0.5, parseFloat(((await page1).getPageWidth()).toString()) - 190, parseFloat(((await page1).getPageHeight()).toString()) - 775);
 
             (await builder).createImage
             ;(await writer).writeElement(await element);
 
             fs.writeFileSync('./my-qr-code.png', qr_svg);
-            let img = PDFNet.Image.createFromFile(this.doc, './my-qr-code.png');
+            let img = PDFNet.Image.createFromFile(doc, './my-qr-code.png');
 
             element = (await builder).createImageScaled(await img, 300, 600, 200, -150);
             (await writer).writeElement(await element);
             (await writer).writeElement(await (await builder).createTextEnd());
 
             (await writer).end(); // save changes to the current page
-            this.doc.pageRemove(this.doc.getPageIterator(1));
-            this.doc.pagePushBack(page1);
+            doc.pageRemove(await doc.getPageIterator(1));
+            doc.pagePushBack(await page1);
 
 
 
-            this.doc.save('mm.pdf', PDFNet.SDFDoc.SaveOptions.e_remove_unused);
+            doc.save('mm.pdf', PDFNet.SDFDoc.SaveOptions.e_remove_unused);
 
             PDFNet.runWithCleanup(this.Sign, 'demo:omaralami230@gmail.com:7b01f4ab020000000092768e068e8737e8b8c939452e7892e0470df170');
         }
@@ -94,13 +95,13 @@ class DocumentSign {
         await PDFNet.initialize('demo:omaralami230@gmail.com:7b01f4ab020000000092768e068e8737e8b8c939452e7892e0470df170');
         let in_docpath = 'mm.pdf';
         let in_public_key_file_path = this.certificate;
-        let doc1 = PDFNet.PDFDoc.createFromFilePath(in_docpath);
-        (await doc1).initSecurityHandler();
+        let doc1 = await PDFNet.PDFDoc.createFromFilePath(in_docpath);
+        doc1.initSecurityHandler();
         console.log('==========');
         let opts = PDFNet.VerificationOptions.create(PDFNet.VerificationOptions.SecurityLevel.e_compatibility_and_archiving);
         (await opts).addTrustedCertificateUString(in_public_key_file_path, PDFNet.VerificationOptions.CertificateTrustFlag.e_default_trust + PDFNet.VerificationOptions.CertificateTrustFlag.e_certification_trust);
         // Add trust root to store of trusted certificates contained in VerificationOptions.
-        let digsig_fitr = (await doc1).getDigitalSignatureFieldIteratorBegin();
+        let digsig_fitr = doc1.getDigitalSignatureFieldIteratorBegin();
         console.log(digsig_fitr);
         var verification_status = true;
         for (; (await digsig_fitr).hasNext(); (await digsig_fitr).next()) {
